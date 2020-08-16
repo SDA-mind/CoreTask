9695372dcc547ac30ddb1bd7e9765e69a59df183
SDA-mind <dima-sherbakov@mail.ru>
2020-08-16 23:58:07 +0300

    2

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,19 @@
+package jm.task.core.jdbc;
+
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Tom", "Brook", (byte) 25);
+        userService.saveUser("Bob", "Green", (byte) 30);
+        userService.saveUser("Clara", "Banks", (byte) 20);
+        userService.saveUser("Kent", "White", (byte) 15);
+        userService.getAllUsers();
+        userService.removeUserById(2);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
