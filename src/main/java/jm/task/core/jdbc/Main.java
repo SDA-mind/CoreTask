d6464bdc7bfa1b0599ab34f53906a10438f50463
SDA-mind <dima-sherbakov@mail.ru>
2020-08-17 15:24:39 +0300

    second

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,26 @@
 package jm.task.core.jdbc;
 
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Tom", "Brook", (byte) 25);
-        userService.saveUser("Bob", "Green", (byte) 30);
-        userService.saveUser("Clara", "Banks", (byte) 20);
-        userService.saveUser("Kent", "White", (byte) 15);
-        userService.removeUserById(2);
-        userService.getAllUsers();
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        try {
+            UserService userService = new UserServiceImpl();
+            userService.createUsersTable();
+            userService.saveUser("Tom", "Brook", (byte) 25);
+            userService.saveUser("Bob", "Green", (byte) 30);
+            userService.saveUser("Clara", "Banks", (byte) 20);
+            userService.saveUser("Kent", "White", (byte) 15);
+            userService.removeUserById(2);
+            userService.getAllUsers();
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+        } catch (SQLException e) {
+
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDaoJDBC;
+    private UserDao userDaoJDBC;
 
     public UserServiceImpl() {
         userDaoJDBC = new UserDaoJDBCImpl();
